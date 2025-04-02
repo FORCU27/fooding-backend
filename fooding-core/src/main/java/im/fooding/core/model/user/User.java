e2655718fbf4e7a0ea8edb0cbc27e7eca8b2ca4a
SooMin <pepsi_soomae@naver.com>
2025-04-03 03:28:37 +0900

    refactor: User 도메인 코드 개선 및 일부 기능 추가

--- fooding-core/src/main/java/im/fooding/core/model/user/User.java
+++ fooding-core/src/main/java/im/fooding/core/model/user/User.java
@@ -7,7 +7,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
@@ -15,61 +17,92 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DynamicUpdate
+@Table(name = "users")
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     private String password;
 
+    @Column
+    private String provider;
+
+    @Column(nullable = false, length = 20)
     private String nickname;
 
-    private String mobile;
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(unique = true, length = 20)
+    private String referralCode;
+
+    private String profileImage;
 
     private String refreshToken;
 
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private int loginCount;
 
-    private LocalDateTime lastLoginAt;
+    private LocalDateTime lastLoggedInAt;
 
     @Column(nullable = false)
     @ColumnDefault("true")
-    private boolean termsAgree;
+    private boolean termsAgreed;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime termsAgreedAt;
 
     @Column(nullable = false)
     @ColumnDefault("true")
-    private boolean privacyAgree;
+    private boolean privacyPolicyAgreed;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime privacyPolicyAgreedAt;
 
     @Column(nullable = false)
     @ColumnDefault("false")
-    private boolean marketingAgree;
+    private boolean marketingConsent;
+
+    private LocalDateTime marketingConsentAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
 
     @Builder
-    public User(Role role, String email, String password, String nickname, String mobile, boolean termsAgree, boolean privacyAgree, boolean marketingAgree) {
+    public User(Role role, String email, String password, String provider, String nickname, String phoneNumber, boolean termsAgreed, boolean privacyPolicyAgreed, boolean marketingConsent, Gender gender) {
         this.role = role;
         this.email = email;
         this.password = password;
+        this.provider = provider;
         this.nickname = nickname;
-        this.mobile = mobile;
-        this.termsAgree = termsAgree;
-        this.privacyAgree = privacyAgree;
-        this.marketingAgree = marketingAgree;
+        this.phoneNumber = phoneNumber;
+        this.termsAgreed = termsAgreed;
+        this.privacyPolicyAgreed = privacyPolicyAgreed;
+        this.marketingConsent = marketingConsent;
+        this.gender = gender;
     }
 
     public void updateRefreshToken(String updateRefreshToken) {
         this.refreshToken = updateRefreshToken;
-        this.lastLoginAt = LocalDateTime.now();
         this.loginCount++;
+        this.lastLoggedInAt = LocalDateTime.now();
     }
 
-    public void update(String nickname) {
+    public void update(String password, String nickname, String phoneNumber, String profileImage) {
+        this.password = password;
         this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.profileImage = profileImage;
     }
 }
