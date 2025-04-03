e8292cf06246fd85372e4ebd8c148a3631e87d26
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-04-03 19:02:43 +0900

    feat: Device 도메인 추가, StoreDevice 도메인 추가

---
+++ fooding-core/src/main/java/im/fooding/core/model/device/DeviceType.java
@@ -0,0 +1,10 @@
+package im.fooding.core.model.device;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum DeviceType {
+    IOS, ANDROID
+}
