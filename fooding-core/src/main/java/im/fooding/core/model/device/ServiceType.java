d33e9bbdf36f9f8d4c3f23cd349ae227e5d23bd5
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-04-03 18:44:52 +0900

    feat: Device 도메인 추가, StoreDevice 도메인 추가

---
+++ fooding-core/src/main/java/im/fooding/core/model/device/ServiceType.java
@@ -0,0 +1,5 @@
+package im.fooding.core.model.device;
+
+public enum ServiceType {
+    REWARD_MANAGEMENT, REWARD_RECEIPT, WAITING_MANAGEMENT, WAITING_RECEIPT
+}
---
+++ fooding-core/src/main/java/im/fooding/core/model/device/StoreDevice.java
@@ -0,0 +1,40 @@
+package im.fooding.core.model.device;
+
+import im.fooding.core.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+public class StoreDevice extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "store_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+//    private Store store;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Device device;
+
+    @Column( name = "service_type" )
+    @Enumerated( EnumType.STRING )
+    private ServiceType serviceType;
+
+    @Column( name = "last_connected_at" )
+    private LocalDateTime lastConnectedAt;
+
+    public void updateLastConnected( LocalDateTime lastConnectedAt ){
+        this.lastConnectedAt = lastConnectedAt;
+    }
+
+}
