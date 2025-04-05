ca7636f8eb6fa53ad4454e1c21611a593f7493e0
Jeongyounghyeon <snorlax1106@naver.com>
2025-04-06 03:06:56 +0900

    feat: BE-16 WaitingRequest 도메인 추가

---
+++ fooding-core/src/main/java/im/fooding/core/model/waiting/WaitingUser.java
@@ -0,0 +1,65 @@
+package im.fooding.core.model.waiting;
+
+import im.fooding.core.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+// todo: Store 객체 추가
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Table(name = "waiting_users")
+public class WaitingUser extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "store_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+//    private Store store;
+
+    private String name;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private boolean termsAgreed;
+
+    @Column(nullable = false)
+    private boolean privacyPolicyAgreed;
+
+    @Column(nullable = false)
+    private boolean thirdPartyAgreed;
+
+    @Column(nullable = false)
+    private boolean marketingConsent;
+
+    @Column(nullable = false)
+    private int count;
+
+//    @Builder
+//    public WaitingUser(Store store, String name, String phoneNumber, boolean termsAgreed, boolean privacyPolicyAgreed, boolean thirdPartyAgreed, boolean marketingConsent) {
+//        this.name = name;
+//        this.phoneNumber = phoneNumber;
+//        this.termsAgreed = termsAgreed;
+//        this.privacyPolicyAgreed = privacyPolicyAgreed;
+//        this.thirdPartyAgreed = thirdPartyAgreed;
+//        this.marketingConsent = marketingConsent;
+//        this.count = 0;
+//    }
+
+    public void visitStore() {
+        count++;
+    }
+}
