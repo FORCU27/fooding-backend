00b8a4332d47df85a8b8c7583dba2ec9948da48c
baekjaehyuk <bjh010321@naver.com>
2025-04-07 20:55:12 +0900

    feat: Store 도메인 추가

---
+++ fooding-core/src/main/java/im/fooding/core/model/store/Store.java
@@ -0,0 +1,68 @@
+package im.fooding.core.model.store;
+
+import im.fooding.core.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Table(name = "stores")
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "store_number", nullable = false)
+    private String storeNumber;
+
+    @Column(name = "home_page")
+    private String homePage;
+
+    @Column(nullable = false)
+    private String direction;
+
+    @Column(nullable = false)
+    private String storeInfo;
+
+    @Column(nullable = false)
+    @ColumnDefault("false")
+    private boolean isParkingAvailable;
+
+    @Builder
+    public Store(
+            String name,
+            String description,
+            String storeNumber,
+            String homePage,
+            String direction,
+            String storeInfo,
+            boolean isParkingAvailable
+    ) {
+        this.name = name;
+        this.description = description;
+        this.storeNumber = storeNumber;
+        this.homePage = homePage;
+        this.direction = direction;
+        this.storeInfo = storeInfo;
+        this.isParkingAvailable = isParkingAvailable;
+    }
+}
