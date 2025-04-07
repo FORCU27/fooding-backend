2c51eda12eb629ba675a936aeb03f6665847d442
baekjaehyuk <bjh010321@naver.com>
2025-04-07 20:56:02 +0900

    feat: StoreTag 도메인 추가

---
+++ fooding-core/src/main/java/im/fooding/core/model/store/StoreTag.java
@@ -0,0 +1,48 @@
+package im.fooding.core.model.store;
+
+import im.fooding.core.model.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+@Table(name = "store_tags")
+public class StoreTag extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "store_id",
+            nullable = false,
+            foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    private Store store;
+
+    @Column(name = "tag")
+    private String tag;
+
+    @Builder
+    public StoreTag(Store store, String tag) {
+        this.store = store;
+        this.tag = tag;
+    }
+}
