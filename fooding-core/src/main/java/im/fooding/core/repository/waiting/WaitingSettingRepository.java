3f3fab963b7315c432e6851b29abdd0621fbf294
Jeongyounghyeon <snorlax1106@naver.com>
2025-04-19 23:38:33 +0900

    feat: 웨이팅 세팅 레포지토리의 상속 관계 변경

--- fooding-core/src/main/java/im/fooding/core/repository/waiting/WaitingSettingRepository.java
+++ fooding-core/src/main/java/im/fooding/core/repository/waiting/WaitingSettingRepository.java
@@ -1,14 +1,7 @@
 package im.fooding.core.repository.waiting;
 
-import im.fooding.core.model.store.Store;
 import im.fooding.core.model.waiting.WaitingSetting;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface WaitingSettingRepository extends JpaRepository<WaitingSetting, Long> {
-
-    @Query("SELECT ws FROM WaitingSetting ws WHERE ws.waiting.store = :store AND ws.isActive = true")
-    Optional<WaitingSetting> findActive(@Param("store") Store store);
+public interface WaitingSettingRepository extends JpaRepository<WaitingSetting, Long>, QWaitingSettingRepository {
 }
