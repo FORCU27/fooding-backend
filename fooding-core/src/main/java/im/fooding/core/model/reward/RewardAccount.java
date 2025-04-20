6d6c67912efa4ff069c04d3c39e16b92d1c67c76
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-04-20 10:52:51 +0900

    feat: RewardAccount 도메인 모델링

--- fooding-core/src/main/java/im/fooding/core/model/reward/RewardAccount.java
+++ fooding-core/src/main/java/im/fooding/core/model/reward/RewardAccount.java
@@ -1,7 +1,7 @@
 package im.fooding.core.model.reward;
 
-import im.fooding.core.model.BaseEntity;
 import im.fooding.core.model.store.Store;
+import im.fooding.core.model.user.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -12,8 +12,8 @@ import org.hibernate.annotations.DynamicUpdate;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DynamicUpdate
-@Table( name = "reward_log" )
-public class RewardLog extends BaseEntity {
+@Table( name = "reward_account" )
+public class RewardAccount {
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
@@ -22,26 +22,17 @@ public class RewardLog extends BaseEntity {
     @JoinColumn( name = "store_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT) )
     private Store store;
 
-//    추후 StoreServiceId 관련 백로그가 merge되면 추가 예정
-//    @ManyToOne
-//    @JoinColumn( name = "service_id" )
-//    private StoreService serviceId;
-
     @Column( name = "phone_number" )
     private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn( name = "user_id" )
+    private User user;
 
     @Column( name = "point" )
     private int point;
 
-    @Column( name =  "status" )
-    @Enumerated( EnumType.STRING )
-    private RewardStatus status;
-
-    @Column( name = "type" )
-    @Enumerated( EnumType.STRING )
-    private RewardType type;
-
-    @Column( name = "channel" )
-    @Enumerated( EnumType.STRING )
-    private RewardChannel channel;
+    public void addPoint( int earnPoint ){
+        this.point += earnPoint;
+    }
 }
