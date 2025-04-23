4ac55c6cffe0bb55ed5f518b03f4f35f59094f4c
SooMin <pepsi_soomae@naver.com>
2025-04-23 20:47:43 +0900

    feat: StorePost 도메인 모델링 추가

---
+++ fooding-core/src/main/java/im/fooding/core/model/store/StorePost.java
@@ -0,0 +1,58 @@
+package im.fooding.core.model.store;
+
+import im.fooding.core.common.StringListConverter;
+import im.fooding.core.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicUpdate
+public class StorePost extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Store store;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Convert(converter = StringListConverter.class)
+    @Column(name = "tags")
+    private List<String> tags;
+
+    @ColumnDefault("false")
+    @Column(nullable = false)
+    private boolean isFixed;
+
+    @Builder
+    public StorePost(Store store, String title, String content, List<String> tags, boolean isFixed) {
+      this.store = store;
+      this.title = title;
+      this.content = content;
+      this.tags = tags;
+      this.isFixed = isFixed;
+    }
+
+    public void update(String title, String content, List<String> tags, boolean isFixed) {
+      this.title = title;
+      this.content = content;
+      this.tags = tags;
+      this.isFixed = isFixed;
+    }
+}
