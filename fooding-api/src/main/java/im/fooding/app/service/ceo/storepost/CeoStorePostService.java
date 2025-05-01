6382cf265d960f99ed09cb64d1636caec2a3c9e1
SooMin <pepsi_soomae@naver.com>
2025-05-01 23:58:11 +0900

    feat: 점주 소식 생성, 수정, 삭제 API 추가

--- fooding-api/src/main/java/im/fooding/app/service/ceo/storepost/CeoStorePostService.java
+++ fooding-api/src/main/java/im/fooding/app/service/ceo/storepost/CeoStorePostService.java
@@ -1,8 +1,12 @@
 package im.fooding.app.service.ceo.storepost;
 
+import im.fooding.app.dto.request.ceo.storepost.CeoCreateStorePostRequest;
+import im.fooding.app.dto.request.ceo.storepost.CeoUpdateStorePostRequest;
 import im.fooding.app.dto.response.ceo.storepost.StorePostResponse;
+import im.fooding.core.model.store.Store;
 import im.fooding.core.model.store.StorePost;
 import im.fooding.core.service.store.StorePostService;
+import im.fooding.core.service.store.StoreService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,11 +21,40 @@ import java.util.stream.Collectors;
 @Slf4j
 public class CeoStorePostService {
     private final StorePostService storePostService;
+    private final StoreService storeService;
 
     public List<StorePostResponse> list(Long storeId) {
       List<StorePost> storePosts = storePostService.list(storeId);
       return storePosts.stream()
               .map(StorePostResponse::from)
               .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Long create(CeoCreateStorePostRequest request) {
+      Store store = storeService.findById(request.getStoreId());
+
+      StorePost storePost = StorePost.builder()
+              .store(store)
+              .title(request.getTitle())
+              .content(request.getContent())
+              .tags(request.getTags())
+              .isFixed(request.getIsFixed())
+              .build();
+
+      return storePostService.create(storePost).getId();
+
+    }
+
+    @Transactional
+    public void update(Long storePostId, CeoUpdateStorePostRequest request) {
+      StorePost storePost = storePostService.findById(storePostId);
+      storePostService.update(storePost, request.getTitle(), request.getContent(), request.getTags(), request.getIsFixed());
+
+    }
+
+    @Transactional
+    public void delete(Long storePostId, Long deletedBy) {
+      storePostService.delete(storePostId, deletedBy);
     }
 }
