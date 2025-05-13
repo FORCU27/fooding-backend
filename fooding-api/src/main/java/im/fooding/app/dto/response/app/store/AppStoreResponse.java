09057c07e0760eb901354ed19c5cb7cd1473122c
Jeongyounghyeon <snorlax1106@naver.com>
2025-05-13 16:23:03 +0900

    feat: swagger 수정

--- fooding-api/src/main/java/im/fooding/app/dto/response/app/store/AppStoreResponse.java
+++ fooding-api/src/main/java/im/fooding/app/dto/response/app/store/AppStoreResponse.java
@@ -5,46 +5,46 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record AppStoreResponse(
 
-        @Schema(name = "id")
+        @Schema(description = "id")
         Long id,
 
-        @Schema(name = "가게 이름")
+        @Schema(description = "가게 이름")
         String name,
 
-        @Schema(name = "매장 위치")
+        @Schema(description = "매장 위치")
         String city,
 
-        @Schema(name = "매장 주소")
+        @Schema(description = "매장 주소")
         String address,
 
-        @Schema(name = "매장 카테코리")
+        @Schema(description = "매장 카테코리")
         String category,
 
-        @Schema(name = "매장 소개")
+        @Schema(description = "매장 소개")
         String description,
 
-        @Schema(name = "평균 가격대")
+        @Schema(description = "평균 가격대")
         String priceCategory,
 
-        @Schema(name = "매장 이벤트 소개")
+        @Schema(description = "매장 이벤트 소개")
         String eventDescription,
 
-        @Schema(name = "매장 전화번호")
+        @Schema(description = "매장 전화번호")
         String contactNumber,
 
-        @Schema(name = "찾아오는 방법")
+        @Schema(description = "찾아오는 방법")
         String direction,
 
-        @Schema(name = "안내 및 유의 사항")
+        @Schema(description = "안내 및 유의 사항")
         String information,
 
-        @Schema(name = "주차 가능 여부")
+        @Schema(description = "주차 가능 여부")
         boolean isParkingAvailable,
 
-        @Schema(name = "매장 신규오픈 여부")
+        @Schema(description = "매장 신규오픈 여부")
         boolean isNewOpen,
 
-        @Schema(name = "매장 테이크아웃 여부")
+        @Schema(description = "매장 테이크아웃 여부")
         boolean isTakeOut
 ) {
 
