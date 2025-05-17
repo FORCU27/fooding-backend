e4b24bef420856a3080f60893b7b5445eadffa2d
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-05-17 15:39:55 +0900

    [BE-97] APP에서 가게별 가입 서비스 조회 API 제작 (#72)
    * feat: APP에서 가게별 가입 서비스 조회 API 제작
    
    * fix: 경로 가시성을 위해 - 추가

--- fooding-core/src/main/java/im/fooding/core/repository/store/StoreServiceRepository.java
+++ fooding-core/src/main/java/im/fooding/core/repository/store/StoreServiceRepository.java
@@ -3,5 +3,8 @@ package im.fooding.core.repository.store;
 import im.fooding.core.model.store.StoreService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StoreServiceRepository extends JpaRepository<StoreService, Long>, QStoreServiceRepository {
+    List<StoreService> findByStoreId( Long storeId );
 }
