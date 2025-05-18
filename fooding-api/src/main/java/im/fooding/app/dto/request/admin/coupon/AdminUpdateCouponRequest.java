8a949ede62a917650b5ea79799e8fed671f704b9
Jin <hmjin11@gmail.com>
2025-05-18 19:49:18 +0900

    refactor: swagger requiredMode 불필요로 롤백

--- fooding-api/src/main/java/im/fooding/app/dto/request/admin/coupon/AdminUpdateCouponRequest.java
+++ fooding-api/src/main/java/im/fooding/app/dto/request/admin/coupon/AdminUpdateCouponRequest.java
@@ -2,7 +2,6 @@ package im.fooding.app.dto.request.admin.coupon;
 
 import im.fooding.core.model.coupon.*;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -16,53 +15,53 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public class AdminUpdateCouponRequest {
-    @Schema(description = "store id", requiredMode = RequiredMode.NOT_REQUIRED, example = "1")
+    @Schema(description = "store id", example = "1")
     private Long storeId;
 
     @NotNull
-    @Schema(description = "혜택 타입", requiredMode = RequiredMode.REQUIRED, example = "DISCOUNT")
+    @Schema(description = "혜택 타입", example = "DISCOUNT")
     private BenefitType benefitType;
 
     @NotNull
-    @Schema(description = "쿠폰 타입", requiredMode = RequiredMode.REQUIRED, example = "FIRST_COME_FIRST_SERVED")
+    @Schema(description = "쿠폰 타입", example = "FIRST_COME_FIRST_SERVED")
     private CouponType type;
 
     @NotNull
-    @Schema(description = "할인 타입", requiredMode = RequiredMode.REQUIRED, example = "FIXED")
+    @Schema(description = "할인 타입", example = "FIXED")
     private DiscountType discountType;
 
     @NotNull
-    @Schema(description = "제공 대상", requiredMode = RequiredMode.REQUIRED, example = "ALL")
+    @Schema(description = "제공 대상", example = "ALL")
     private ProvideType provideType;
 
     @NotBlank
     @Size(max = 50)
-    @Schema(description = "쿠폰명", requiredMode = RequiredMode.REQUIRED, example = "2000원 할인쿠폰")
+    @Schema(description = "쿠폰명", example = "2000원 할인쿠폰")
     private String name;
 
     @Size(max = 200)
-    @Schema(description = "사용 조건", requiredMode = RequiredMode.NOT_REQUIRED, example = "메뉴 2개 이상 시킬시 사용 가능합니다.")
+    @Schema(description = "사용 조건", example = "메뉴 2개 이상 시킬시 사용 가능합니다.")
     private String conditions;
 
-    @Schema(description = "총 발급수량", requiredMode = RequiredMode.NOT_REQUIRED, example = "50")
+    @Schema(description = "총 발급수량", example = "50")
     private Integer totalQuantity;
 
     @Positive
-    @Schema(description = "할인값(금액, 퍼센트)", requiredMode = RequiredMode.REQUIRED, example = "2000")
+    @Schema(description = "할인값(금액, 퍼센트)", example = "2000")
     private int discountValue;
 
     @NotNull
-    @Schema(description = "발급시작일", requiredMode = RequiredMode.REQUIRED, example = "2025-05-01")
+    @Schema(description = "발급시작일", example = "2025-05-01")
     private LocalDate issueStartOn;
 
-    @Schema(description = "발급마감일", requiredMode = RequiredMode.NOT_REQUIRED, example = "2025-05-31")
+    @Schema(description = "발급마감일", example = "2025-05-31")
     private LocalDate issueEndOn;
 
-    @Schema(description = "사용기한", requiredMode = RequiredMode.NOT_REQUIRED, example = "2025-12-31")
+    @Schema(description = "사용기한", example = "2025-12-31")
     private LocalDate expiredOn;
 
     @NotNull
-    @Schema(description = "상태", requiredMode = RequiredMode.REQUIRED, example = "ACTIVE")
+    @Schema(description = "상태", example = "ACTIVE")
     private CouponStatus status;
 
     @Builder
--- fooding-api/src/main/java/im/fooding/app/dto/request/auth/AuthLoginRequest.java
+++ fooding-api/src/main/java/im/fooding/app/dto/request/auth/AuthLoginRequest.java
@@ -2,7 +2,6 @@ package im.fooding.app.dto.request.auth;
 
 import im.fooding.core.model.user.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -15,16 +14,16 @@ import lombok.NoArgsConstructor;
 public class AuthLoginRequest {
     @NotBlank
     @Size(max = 50)
-    @Schema(description = "아이디", requiredMode = RequiredMode.REQUIRED, example = "admin")
+    @Schema(description = "아이디", example = "admin")
     private String email;
 
     @NotBlank
     @Size(min = 4, max = 20)
-    @Schema(description = "비밀번호", requiredMode = RequiredMode.REQUIRED, example = "1234")
+    @Schema(description = "비밀번호", example = "1234")
     private String password;
 
     @NotNull
-    @Schema(description = "role", requiredMode = RequiredMode.REQUIRED, example = "ADMIN")
+    @Schema(description = "role", example = "ADMIN")
     private Role role;
 
     @Builder
