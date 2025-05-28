26e278d8f541c9e4c82c4e4cddc1bb981f1143b1
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-05-28 20:43:40 +0900

    feat: BE-96 ADMIN 리뷰 CRUD

---
+++ fooding-api/src/main/java/im/fooding/app/dto/request/admin/review/AdminUpdateReviewRequest.java
@@ -0,0 +1,25 @@
+package im.fooding.app.dto.request.admin.review;
+
+import im.fooding.core.model.review.VisitPurposeType;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdminUpdateReviewRequest {
+    @NotNull
+    private Long writerId;
+
+    @NotNull
+    private Long storeId;
+
+    private float serviceScore;
+    private float moodScore;
+    private float tasteScore;
+    private float totalScore;
+
+    private String content;
+
+    private VisitPurposeType visitPurposeType;
+}
--- fooding-core/src/main/java/im/fooding/core/global/exception/ErrorCode.java
+++ fooding-core/src/main/java/im/fooding/core/global/exception/ErrorCode.java
@@ -39,6 +39,9 @@ public enum ErrorCode {
     STORE_IMAGE_NOT_FOUND(HttpStatus.BAD_REQUEST, "2001", "등록된 가게 이미지가 없습니다."),
     STORE_POST_NOT_FOUND(HttpStatus.BAD_REQUEST,"2200", "등록된 가게 소식이 없습니다."),
 
+    // 리뷰
+    REVIEW_NOT_FOUND( HttpStatus.BAD_REQUEST, "2300", "등록된 리뷰가 없습니다" ),
+
 
     // 웨이팅
     WAITING_NOT_FOUND(HttpStatus.BAD_REQUEST, "3000", "등록된 웨이팅 정보가 없습니다."),
@@ -66,6 +69,7 @@ public enum ErrorCode {
     REWARD_POINT_NOT_ENOUGH( HttpStatus.BAD_REQUEST, "4001", "리워드 누적 포인트가 부족합니다." ),
 
     REWARD_LOG_NOT_FOUND( HttpStatus.BAD_REQUEST, "4100", "해당 로그가 존재하지 않습니다" ),
+
     // 디바이스
     DEVICE_NOT_FOUND(HttpStatus.BAD_REQUEST, "7000", "등록된 디바이스가 없습니다."),
     
