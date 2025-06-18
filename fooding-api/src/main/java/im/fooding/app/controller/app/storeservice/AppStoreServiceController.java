f7289bd6664753dd0f1b9fd718181c4c24f0bb22
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-06-18 17:56:43 +0900

    feat: 유저 아이디 정보를 토큰에서 가져오도록 수정

--- fooding-api/src/main/java/im/fooding/app/controller/app/storeservice/AppStoreServiceController.java
+++ fooding-api/src/main/java/im/fooding/app/controller/app/storeservice/AppStoreServiceController.java
@@ -7,6 +7,7 @@ import im.fooding.core.common.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +23,7 @@ public class AppStoreServiceController {
     @Operation( summary = "해당 가게에 대한 가입 스토어 서비스 목록 조회" )
     public ApiResult<List<StoreServiceResponse>> getSignedStoreService(
             @PathVariable Long storeId,
-            @RequestParam Long userId
+            @AuthenticationPrincipal Long userId
     ){
         List<StoreServiceResponse> response = service.list( storeId, userId );
         if( response == null ) return ApiResult.ok();
