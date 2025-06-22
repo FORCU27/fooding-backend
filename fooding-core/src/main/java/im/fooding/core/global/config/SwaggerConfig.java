023b4b815f8aba00f82230168bf414d6583c927e
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-06-22 20:57:17 +0900

    feat: Swagger에서 Bearer Token 인증할 수 있는 기능 추가

--- fooding-core/src/main/java/im/fooding/core/global/config/SwaggerConfig.java
+++ fooding-core/src/main/java/im/fooding/core/global/config/SwaggerConfig.java
@@ -1,17 +1,27 @@
 package im.fooding.core.global.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
                 .components(new Components())
                 .info(apiInfo());
     }
