0a36545f3a87b8f5fa727cce0f73e0792c1fe95a
Jeongyounghyeon <snorlax1106@naver.com>
2025-06-29 17:09:00 +0900

    feat: 지역 레포지토리 생성

---
+++ fooding-core/src/main/java/im/fooding/core/repository/region/RegionRepository.java
@@ -0,0 +1,11 @@
+package im.fooding.core.repository.region;
+
+import im.fooding.core.model.region.Region;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, String> {
+
+    Page<Region> findAllByDeletedFalse(Pageable pageable);
+}
