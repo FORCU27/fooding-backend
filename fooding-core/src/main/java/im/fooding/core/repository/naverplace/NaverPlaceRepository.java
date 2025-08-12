707afa9ac92a94fd4ff8a4196d2e33036b691bf7
Jeongyounghyeon <jeongyounghyeon1106@gmail.com>
2025-08-12 14:06:49 +0900

    feat: 네이버 플레이스 도메인 및 레포지토리 추가

---
+++ fooding-core/src/main/java/im/fooding/core/repository/naverplace/NaverPlaceRepository.java
@@ -0,0 +1,7 @@
+package im.fooding.core.repository.naverplace;
+
+import im.fooding.core.model.naverplace.NaverPlace;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NaverPlaceRepository extends MongoRepository<NaverPlace, String> {
+}
