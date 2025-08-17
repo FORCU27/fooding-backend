412389a44cd5ca23d4d08fa3e868a04a7855b0f7
Jeongyounghyeon <jeongyounghyeon1106@gmail.com>
2025-08-17 12:46:26 +0900

    feat: [User] 지역 조회 API 추가

---
+++ fooding-api/src/main/java/im/fooding/app/dto/request/user/region/UserRegionListRequest.java
@@ -0,0 +1,8 @@
+package im.fooding.app.dto.request.user.region;
+
+import im.fooding.core.common.BasicSearch;
+import lombok.Value;
+
+@Value
+public class UserRegionListRequest extends BasicSearch {
+}
