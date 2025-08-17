e171b27aea609b06137800516ebaa8dc1917fd0a
Jeongyounghyeon <jeongyounghyeon1106@gmail.com>
2025-08-17 13:38:17 +0900

    feat: User 온라인 웨이팅 API 추가

--- fooding-api/src/main/java/im/fooding/app/controller/user/waiting/UserStoreWaitingController.java
+++ fooding-api/src/main/java/im/fooding/app/controller/user/waiting/UserStoreWaitingController.java
@@ -1,13 +1,19 @@
 package im.fooding.app.controller.user.waiting;
 
+import im.fooding.app.dto.request.user.waiting.UserStoreWaitingRegisterRequest;
+import im.fooding.app.dto.response.user.waiting.UserStoreWaitingCreateResponse;
 import im.fooding.app.dto.response.user.waiting.UserStoreWaitingResponse;
 import im.fooding.app.service.user.waiting.UserStoreWaitingService;
 import im.fooding.core.common.ApiResult;
+import im.fooding.core.global.UserInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +29,15 @@ public class UserStoreWaitingController {
     @Operation(summary = "스토어 웨이팅 조회")
     public ApiResult<UserStoreWaitingResponse> getStoreWaiting(@PathVariable long id) {
         return ApiResult.ok(userWaitingService.getStoreWaiting(id));
+    }
+
+
+    @PostMapping
+    @Operation(summary = "온라인 웨이팅 접수")
+    public ApiResult<UserStoreWaitingCreateResponse> registerStoreWaiting(
+            @RequestBody UserStoreWaitingRegisterRequest request, @AuthenticationPrincipal UserInfo userInfo
+
+    ) {
+        return ApiResult.ok(userWaitingService.registerStoreWaiting(request, userInfo.getId()));
     }
 }
