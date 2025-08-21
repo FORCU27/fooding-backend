05365275f1121b9885897f5773fafea8d646630f
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-08-21 16:26:06 +0900

    feat: API 추가 및 수정, 작동 확인 및 테스트 필요

--- fooding-api/src/main/java/im/fooding/app/controller/pos/reward/PosRewardController.java
+++ fooding-api/src/main/java/im/fooding/app/controller/pos/reward/PosRewardController.java
@@ -1,9 +1,11 @@
 package im.fooding.app.controller.pos.reward;
 
-import im.fooding.app.dto.request.pos.reward.GetRewardRequest;
+import im.fooding.app.dto.request.pos.reward.GetPosRewardRequest;
+import im.fooding.app.dto.request.pos.reward.UpdateRewardLogRequest;
 import im.fooding.app.dto.response.pos.reward.GetPosRewardResponse;
 import im.fooding.app.service.pos.reward.PosRewardService;
 import im.fooding.core.common.ApiResult;
+import im.fooding.core.common.PageResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,18 +23,40 @@ public class PosRewardController {
 
     @GetMapping()
     @Operation( summary = "특정 스토어의 리워드 적립 조회" )
-    public ApiResult<Page<GetPosRewardResponse>> list(
-            @ModelAttribute GetRewardRequest request
+    public ApiResult<PageResponse<GetPosRewardResponse>> list(
+            @RequestBody GetPosRewardRequest request
     ){
         return ApiResult.ok( service.list( request ) );
     }
 
-    @PostMapping("/{id}/cancel")
+    @PatchMapping("/{id}/cancel")
     @Operation( summary = "특정 리워드 로그에 대한 적립 취소" )
     public ApiResult<Void> cancelReward(
-            @PathVariable Long id
-    ){
-        service.cancel( id );
+            @PathVariable Long id,
+            @RequestBody UpdateRewardLogRequest request
+            ){
+        service.cancel( id, request );
         return ApiResult.ok();
     }
+
+    @PatchMapping( "/{id}/approve" )
+    @Operation( summary = "특정 리워드 로그에 대한 적립 허용" )
+    public ApiResult<Void> approveReward(
+            @PathVariable Long id,
+            @RequestBody UpdateRewardLogRequest request
+    ){
+        service.approve( id, request );
+        return ApiResult.ok();
+    }
+
+    @PatchMapping( "/{id}/memo" )
+    @Operation( summary = "특정 리워드 로그에 메모 변경" )
+    public ApiResult<Void> updateMemo(
+            @PathVariable Long id,
+            @RequestBody String memo
+    ){
+        service.updateMemo( id, memo );
+        return ApiResult.ok();
+    }
+
 }
--- fooding-api/src/main/java/im/fooding/app/service/pos/reward/PosRewardService.java
+++ fooding-api/src/main/java/im/fooding/app/service/pos/reward/PosRewardService.java
@@ -1,7 +1,10 @@
 package im.fooding.app.service.pos.reward;
 
-import im.fooding.app.dto.request.pos.reward.GetRewardRequest;
+import im.fooding.app.dto.request.pos.reward.GetPosRewardRequest;
+import im.fooding.app.dto.request.pos.reward.UpdateRewardLogRequest;
 import im.fooding.app.dto.response.pos.reward.GetPosRewardResponse;
+import im.fooding.core.common.PageInfo;
+import im.fooding.core.common.PageResponse;
 import im.fooding.core.model.reward.RewardLog;
 import im.fooding.core.model.reward.RewardStatus;
 import im.fooding.core.service.reward.RewardLogService;
@@ -20,19 +23,35 @@ public class PosRewardService {
     private final RewardLogService logService;
     private final RewardService rewardService;
 
-    public Page<GetPosRewardResponse> list(GetRewardRequest request){
-        return logService.list(
+    public PageResponse<GetPosRewardResponse> list(GetPosRewardRequest request){
+        Page<GetPosRewardResponse> result = logService.list(
                 request.getSearchString(),
                 request.getPageable(),
                 request.getStoreId(),
                 request.getPhoneNumber()
         ).map( GetPosRewardResponse::of );
+        return PageResponse.of( result.stream().toList(), PageInfo.of( result ) );
     }
 
     @Transactional
-    public void cancel( Long rewardLogId ){
+    public void cancel(Long rewardLogId, UpdateRewardLogRequest request){
         RewardLog log = logService.findById( rewardLogId );
         log.updateStatus( RewardStatus.CANCELED );
+        log.updateMemo( request.getMemo() );
         rewardService.usePoint(log.getPhoneNumber(), log.getStore().getId(), log.getPoint());
+    }
+
+    @Transactional
+    public void approve( Long rewardLogId, UpdateRewardLogRequest request ){
+        RewardLog log = logService.findById( rewardLogId );
+        log.updateStatus( RewardStatus.EARNED );
+        log.updateMemo( request.getMemo() );
+        rewardService.addPoint( log.getPhoneNumber(), log.getStore().getId(), log.getPoint() );
+    }
+
+    @Transactional
+    public void updateMemo( Long id, String memo ){
+        RewardLog log = logService.findById( id );
+        log.updateMemo( memo );
     }
 }
