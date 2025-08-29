b3415915e266418e79cab1af2c111e159b1890be
titaniper <flutter.kr@gmail.com>
2025-08-29 23:57:47 +0900

    fix: minor

--- fooding-api/src/main/java/im/fooding/app/dto/request/admin/lead/AdminLeadUploadRequest.java
+++ fooding-api/src/main/java/im/fooding/app/dto/request/admin/lead/AdminLeadUploadRequest.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
