462cbb0cdd1128f21a777b335905c29373c93128
titaniper <flutter.kr@gmail.com>
2025-08-29 21:04:49 +0900

    fix: minor

--- fooding-core/src/main/java/im/fooding/core/model/store/Store.java
+++ fooding-core/src/main/java/im/fooding/core/model/store/Store.java
@@ -10,6 +10,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicUpdate;
 
 import java.util.List;
