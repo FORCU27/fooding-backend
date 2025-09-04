3bb9fe29b191d73f2c6131780036218da9f38934
Jeongyounghyeon <jeongyounghyeon1106@gmail.com>
2025-09-05 00:32:48 +0900

    feat: [CEO] 메뉴 CRUD API 추가

---
+++ fooding-api/src/main/java/im/fooding/app/dto/request/ceo/menu/CeoMenuCreateRequest.java
@@ -0,0 +1,65 @@
+package im.fooding.app.dto.request.ceo.menu;
+
+import im.fooding.core.dto.request.menu.MenuCreateRequest;
+import im.fooding.core.model.menu.MenuCategory;
+import im.fooding.core.model.store.Store;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
+@Value
+@RequiredArgsConstructor
+public class CeoMenuCreateRequest {
+
+    @Schema(description = "가게 ID", example = "1")
+    @NotNull
+    Long storeId;
+
+    @Schema(description = "메뉴 카테고리 ID", example = "1")
+    @NotNull
+    Long categoryId;
+
+    @Schema(description = "메뉴 이름", example = "초밥")
+    @NotNull
+    String name;
+
+    @Schema(description = "메뉴 가격", example = "12000")
+    @NotNull
+    BigDecimal price;
+
+    @Schema(description = "메뉴 설명", example = "맛있는 초밥")
+    @NotNull
+    String description;
+
+    @Schema(description = "이미지 업로드 후 받은 id", example = "819f4bca-2739-46ca-9156-332c86eda619")
+    @NotNull
+    String imageId;
+
+    @Schema(description = "카테고리 정렬", example = "1")
+    @NotNull
+    int sortOrder;
+
+    @Schema(description = "대표 메뉴 여부", example = "true")
+    @NotNull
+    Boolean isSignature;
+
+    @Schema(description = "메뉴 추천 여부", example = "true")
+    @NotNull
+    Boolean isRecommend;
+
+    public MenuCreateRequest toMenuCreateRequest(Store store, MenuCategory category, String imageUrl) {
+        return MenuCreateRequest.builder()
+                .store(store)
+                .category(category)
+                .name(name)
+                .price(price)
+                .description(description)
+                .imageUrl(imageUrl)
+                .sortOrder(sortOrder)
+                .isSignature(isSignature)
+                .isRecommend(isRecommend)
+                .build();
+    }
+}
---
+++ fooding-api/src/main/java/im/fooding/app/dto/request/ceo/menu/CeoMenuListRequest.java
@@ -0,0 +1,13 @@
+package im.fooding.app.dto.request.ceo.menu;
+
+import im.fooding.core.common.BasicSearch;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Value;
+
+@Value
+public class CeoMenuListRequest extends BasicSearch {
+
+    @Schema(description = "가게 ID로 필터", example = "1")
+    Long storeId;
+}
+
