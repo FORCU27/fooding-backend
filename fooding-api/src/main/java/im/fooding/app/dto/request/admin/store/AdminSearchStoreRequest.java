02bd49546f98d27b1cdfc6b4155ef53fa9b0ade0
titaniper <flutter.kr@gmail.com>
2025-09-05 20:22:49 +0900

    feat: admin 필터 추가

--- fooding-api/src/main/java/im/fooding/app/dto/request/admin/store/AdminSearchStoreRequest.java
+++ fooding-api/src/main/java/im/fooding/app/dto/request/admin/store/AdminSearchStoreRequest.java
@@ -1,7 +1,9 @@
 package im.fooding.app.dto.request.admin.store;
 
 import im.fooding.core.common.BasicSearch;
+import im.fooding.core.model.store.StoreCategory;
 import im.fooding.core.model.store.StoreSortType;
+import im.fooding.core.model.store.StoreStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +27,16 @@ public class AdminSearchStoreRequest extends BasicSearch {
             allowableValues = {"ASCENDING", "DESCENDING"}
     )
     private SortDirection sortDirection = SortDirection.DESCENDING;
+
+    @Schema(description = "검색 지역 ID 목록", example = "KR-11,KR-26")
+    private java.util.List<String> regionIds;
+
+    @Schema(description = "카테고리", example = "KOREAN")
+    private StoreCategory category;
+
+    @Schema(description = "조회할 상태들", example = "APPROVED")
+    private java.util.Set<StoreStatus> statuses;
+
+    @Schema(description = "삭제 포함 여부", example = "false")
+    private Boolean includeDeleted = false;
 }
