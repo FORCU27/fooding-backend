33583b5542d569238d943c32b2f78835bac856b9
seongje00416 <seongje00416@gmail.com>
2025-12-01 12:13:56 +0900

    WIP: 테스트를 위한 더미 데이터 생성 API 제작중

--- fooding-api/src/main/java/im/fooding/app/controller/test/TestController.java
+++ fooding-api/src/main/java/im/fooding/app/controller/test/TestController.java
@@ -1,7 +1,9 @@
 package im.fooding.app.controller.test;
 
+import im.fooding.app.service.test.TestService;
 import im.fooding.core.model.test.TestDocument;
 import im.fooding.core.repository.test.TestRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TestController {
     private final TestRepository testRepository;
+    private final TestService testService;
 
     @GetMapping
     public List<TestDocument> getAll() {
@@ -22,5 +25,13 @@ public class TestController {
     @PostMapping
     public void create(@RequestBody TestDocument test) {
         testRepository.save(test);
+    }
+
+    @PostMapping("/store/create/{count}")
+    @Operation( summary = "테스트용 더미 가게 생성 API ( Stage에서 사용 금지 )" )
+    public void createDummyStoreToTest(
+            @PathVariable int count
+    ){
+
     }
 }
