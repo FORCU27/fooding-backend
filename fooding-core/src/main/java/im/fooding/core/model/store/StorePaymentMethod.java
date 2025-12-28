0ab22535441e9c50f5036ff4eabac48a987d6a2d
Jin <hmjin11@gmail.com>
2025-12-28 18:39:38 +0900

    feat: 지역화폐 모바일형, 간편결제 추가

--- fooding-core/src/main/java/im/fooding/core/model/store/StorePaymentMethod.java
+++ fooding-core/src/main/java/im/fooding/core/model/store/StorePaymentMethod.java
@@ -3,5 +3,7 @@ package im.fooding.core.model.store;
 public enum StorePaymentMethod {
     LOCAL_CURRENCY_CARD,   // 지역화폐(카드형)
     LOCAL_CURRENCY_CASH,   // 지역화폐(지류형)
-    ZERO_PAY               // 제로페이
+    LOCAL_CURRENCY_MOBILE, // 지역화폐(모바일형)
+    ZERO_PAY,              // 제로페이
+    MOBILE_PAY             // 간편결제
 }
